caf0d0548f49256c00ed03e85c75c51fb5df00aa
rsl150101 <dnwnd501@naver.com>
2024-10-27 20:14:21 +0900

    [Add] 1.2 WebSocket server using HTTP sever port Redirect all other URLs to "/"

--- src/server.js
+++ src/server.js
@@ -1,21 +1,33 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 const PORT = 5000;
 
-//- view engine set
+//- Set view engine
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views"); //* path set
+app.set("views", __dirname + "/views"); //* Set path
 
-//- static set
-app.use("/public", express.static(__dirname + "/public")); //* static file serve for express middleware
+//- Set static file serving
+app.use("/public", express.static(__dirname + "/public")); //* Serve static files with express middleware
 
-//- router set
-app.get("/", (_, res) => res.render("home")); //* home page render
+//- Set up router
+app.get("/", (_, res) => res.render("home")); //* Render home page
+app.get("/*", (_, res) => res.redirect("/")); //* Redirect all other URLs to "/"
 
-//- server listen
+//- server listen fn
 const handleServerListen = () => {
   console.log(`Listening on http://localhost:${PORT}`);
 };
 
-app.listen(PORT, handleServerListen);
+//! You can create a WebSocket server independently without an HTTP server,
+//! but to run the server on a single port, you should write it like this.
+//- http server
+const server = http.createServer(app);
+
+//- WebSocket server
+const wss = new WebSocket.Server({ server }); //* Create a WebSocket server by passing the HTTP server instance. This allows both servers to operate on the same port.
+
+//- server listen
+server.listen(PORT, handleServerListen);
