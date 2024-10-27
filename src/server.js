c4552ebc86c134262f2358aea9fe8919e144b7e5
rsl150101 <dnwnd501@naver.com>
2024-10-27 17:27:26 +0900

    [Set] view path

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 5000;
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/src/views");
+app.set("views", __dirname + "/views");
 
 const handleServerListen = () => {
   console.log(`Listening on http://localhost:${PORT}`);
