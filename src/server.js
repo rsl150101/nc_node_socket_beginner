6df324d480f4f1e4ebffdaa362676c696e2d2d34
rsl150101 <dnwnd501@naver.com>
2024-10-27 17:52:18 +0900

    [Add] "/" path page render, "/public" path static file serve

--- src/server.js
+++ src/server.js
@@ -3,9 +3,17 @@ import express from "express";
 const app = express();
 const PORT = 5000;
 
+//- view engine set
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+app.set("views", __dirname + "/views"); //* path set
 
+//- static set
+app.use("/public", express.static(__dirname + "/public")); //* static file serve for express middleware
+
+//- router set
+app.get("/", (_, res) => res.render("home")); //* home page render
+
+//- server listen
 const handleServerListen = () => {
   console.log(`Listening on http://localhost:${PORT}`);
 };
