565414b102236c23f718b4e9cb6ee0c92a787eba
rsl150101 <dnwnd501@naver.com>
2024-10-28 19:33:16 +0900

    [Initialize] Day 1 challenge in the 7-day program

---
+++ zoom_clone_challenge_chat/src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import http from "http";
+
+const app = express();
+const PORT = 5000;
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleServerListen = () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+};
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, handleServerListen);
