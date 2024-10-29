62985edf75fd9cb72765061f2b3be7ebe3aaa8fd
rsl150101 <dnwnd501@naver.com>
2024-10-29 18:04:34 +0900

    [Add] 2.1 SocketIO server and bidirectional communication

--- section2_socketio/src/server.js
+++ section2_socketio/src/server.js
@@ -1,10 +1,32 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = 5000;
+
+//- Set view engine
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+//- Set static file serving
+app.use("/public", express.static(__dirname + "/public"));
+
+//- Set up router
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleServerListen = () => {
   console.log(`Listening on http://localhost:${PORT}`);
 };
 
-app.listen(PORT, handleServerListen);
+//- Create server
+const httpServer = http.createServer(app);
+const ioServer = new Server(httpServer);
+
+//-SocketIO server connect
+ioServer.on("connection", (socket) => {
+  console.log(socket);
+});
+
+httpServer.listen(PORT, handleServerListen);
