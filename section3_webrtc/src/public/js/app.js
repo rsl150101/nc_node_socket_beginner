8d60dcfb105b696727a1efc81207376ac98775c0
rsl150101 <dnwnd501@naver.com>
2024-11-02 18:09:44 +0900

    [Add] 3.0 get user video

--- section3_webrtc/src/public/js/app.js
+++ section3_webrtc/src/public/js/app.js
@@ -1,1 +1,46 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+const getMedia = async () => {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+getMedia();
+
+const handleMuteBtnCLick = () => {
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+};
+
+const handleCameraBtnCLick = () => {
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+};
+
+muteBtn.addEventListener("click", handleMuteBtnCLick);
+cameraBtn.addEventListener("click", handleCameraBtnCLick);
