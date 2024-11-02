7e84e4c15b92a5191fffa0358abfb51fc659afc8
rsl150101 <dnwnd501@naver.com>
2024-11-02 16:28:16 +0900

    [Initialize] section3_webrtc settings

---
+++ section3_webrtc/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
+
+const app = express();
+const PORT = 5000;
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleServerListen = () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+};
+
+const httpServer = http.createServer(app);
+const ioServer = SocketIO(httpServer);
+
+ioServer.on("connection", (socket) => {});
+
+httpServer.listen(PORT, handleServerListen);
