b9b88680c1dd9a6ddb80927916c831a923ca3db1
rsl150101 <dnwnd501@naver.com>
2024-11-02 20:10:52 +0900

    [Add] 3.2 customize media request options for audio and video streams

--- section3_webrtc/src/public/js/app.js
+++ section3_webrtc/src/public/js/app.js
@@ -13,10 +13,14 @@ const getCameras = async () => {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const curCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (curCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (error) {
@@ -24,14 +28,25 @@ const getCameras = async () => {
   }
 };
 
-const getMedia = async () => {
+const getMedia = async (deviceId) => {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
+    const initialConstraints = {
       audio: true,
-      video: true,
-    });
+      video: { facingMode: "user" },
+    };
+    const cameraConstraints = {
+      audio: true,
+      video: { deviceId: { exact: deviceId } },
+    };
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !muted));
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !cameraOff));
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -65,5 +80,10 @@ const handleCameraBtnCLick = () => {
   }
 };
 
+const handleCamerChange = async () => {
+  await getMedia(camerasSelect.value);
+};
+
 muteBtn.addEventListener("click", handleMuteBtnCLick);
 cameraBtn.addEventListener("click", handleCameraBtnCLick);
+camerasSelect.addEventListener("change", handleCamerChange);
