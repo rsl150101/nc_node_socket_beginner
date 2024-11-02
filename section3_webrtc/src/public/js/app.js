617f3c4a242d2f1ed838959975e50cfed385214e
rsl150101 <dnwnd501@naver.com>
2024-11-02 19:29:08 +0900

    [Add] 3.1 media on/off control and get user camera device info

--- section3_webrtc/src/public/js/app.js
+++ section3_webrtc/src/public/js/app.js
@@ -3,18 +3,35 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const getMedia = async () => {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
+      audio: true,
       video: true,
     });
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (e) {
     console.log(e);
   }
@@ -23,6 +40,9 @@ const getMedia = async () => {
 getMedia();
 
 const handleMuteBtnCLick = () => {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -33,6 +53,9 @@ const handleMuteBtnCLick = () => {
 };
 
 const handleCameraBtnCLick = () => {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
