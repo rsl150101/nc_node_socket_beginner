a2b2447901934d47cbfd9a4693adfe3ce834a81a
rsl150101 <dnwnd501@naver.com>
2026-04-22 01:17:52 +0900

    [Challenge] Implement P2P video call and real-time chat using WebRTC Data Channels

--- zoom_clone_challenge_webrtc/src/public/js/app.js
+++ zoom_clone_challenge_webrtc/src/public/js/app.js
@@ -1,1 +1,235 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+const welcome = document.getElementById("welcome");
+const call = document.getElementById("call");
+const chat = document.getElementById("chat");
+
+call.hidden = true;
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+/** @type {RTCPeerConnection}*/
+let myPeerConnection;
+let myDatachannel;
+
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const curCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (curCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getMedia = async (deviceId) => {
+  try {
+    const initialConstraints = {
+      audio: true,
+      video: { facingMode: "user" },
+    };
+    const cameraConstraints = {
+      audio: true,
+      video: { deviceId: { exact: deviceId } },
+    };
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !muted));
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !cameraOff));
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const handleMuteBtnCLick = () => {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+};
+
+const handleCameraBtnCLick = () => {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+};
+
+const handleCamerChange = async () => {
+  await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const audioTrack = myStream.getAudioTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    const audioSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "audio");
+    videoSender.replaceTrack(videoTrack);
+    audioSender.replaceTrack(audioTrack);
+  }
+};
+
+muteBtn.addEventListener("click", handleMuteBtnCLick);
+cameraBtn.addEventListener("click", handleCameraBtnCLick);
+camerasSelect.addEventListener("change", handleCamerChange);
+
+//- welcome form
+const welcomeForm = welcome.querySelector("form");
+
+let roomName;
+
+const initCall = async () => {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+};
+
+const handleWelcomeFormSubmit = (e) => {
+  e.preventDefault();
+  const roomNameInput = welcomeForm.querySelector("input");
+  roomName = roomNameInput.value;
+  socket.emit("roomSizeCheck", roomName);
+};
+
+welcomeForm.addEventListener("submit", handleWelcomeFormSubmit);
+
+//- chat form
+const chatForm = chat.querySelector("form");
+
+const addMsg = (msg) => {
+  const chatList = chat.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = msg;
+  chatList.appendChild(li);
+};
+
+const handleChatFormSubmit = (e) => {
+  e.preventDefault();
+  const chatInput = chatForm.querySelector("input");
+  addMsg(`You : ${chatInput.value}`);
+  myDatachannel.send(chatInput.value);
+  chatInput.value = "";
+};
+
+chatForm.addEventListener("submit", handleChatFormSubmit);
+
+//- socket
+socket.on("roomSize", async (size) => {
+  if (size !== 2) {
+    await initCall();
+    socket.emit("joinRoom", roomName);
+  } else {
+    const welcomeFull = welcome.querySelector("h3");
+    welcomeFull.innerText = `${roomName} is full`;
+  }
+});
+
+//* offer side
+socket.on("welcome", async () => {
+  myDatachannel = myPeerConnection.createDataChannel("chat");
+  myDatachannel.addEventListener("message", (event) => {
+    addMsg(`opponent : ${event.data}`);
+  });
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  socket.emit("offer", offer, roomName);
+  myPeerConnection.addEventListener("icecandidate", handleOfferIce);
+});
+
+//* answer side
+socket.on("offer", async (offer) => {
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    myDatachannel = event.channel;
+    myDatachannel.addEventListener("message", (event) => {
+      addMsg(`opponent : ${event.data}`);
+    });
+  });
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  myPeerConnection.addEventListener("icecandidate", handleAnswerIce);
+});
+
+//* offer side
+socket.on("answer", (answer) => {
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+//* answer side
+socket.on("offerIce", (ice) => {
+  myPeerConnection.addIceCandidate(ice); //* Add offer ICE
+});
+
+//* offer side
+socket.on("answerIce", (ice) => {
+  myPeerConnection.addIceCandidate(ice); //* Add answer ICE
+});
+
+//- RTC Code
+const makeConnection = () => {
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun.l.google.com:19302",
+          "stun:stun.l.google.com:19302",
+          "stun:stun.l.google.com:19302",
+        ],
+      },
+    ],
+  });
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+
+  myPeerConnection.addEventListener("track", handlePeerTrack);
+};
+
+const handleOfferIce = (event) => {
+  socket.emit("offerIce", event.candidate, roomName);
+};
+
+const handleAnswerIce = (event) => {
+  socket.emit("answerIce", event.candidate, roomName);
+};
+
+const handlePeerTrack = (event) => {
+  const peerFace = document.querySelector("#peerFace");
+  peerFace.srcObject = event.streams[0];
+};
--- zoom_clone_challenge_webrtc/src/server.js
+++ zoom_clone_challenge_webrtc/src/server.js
@@ -5,6 +5,14 @@ import SocketIO from "socket.io";
 const app = express();
 const PORT = 5000;
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
 const handleServerListen = () => {
   console.log(`Listening on http://localhost:${PORT}`);
 };
@@ -12,6 +20,28 @@ const handleServerListen = () => {
 const httpServer = http.createServer(app);
 const ioServer = SocketIO(httpServer);
 
-ioServer.on("connection", (socket) => {});
+ioServer.on("connection", (socket) => {
+  socket.on("roomSizeCheck", (roomName) => {
+    ioServer
+      .to(socket.id)
+      .emit("roomSize", ioServer.sockets.adapter.rooms.get(roomName)?.size);
+  });
+  socket.on("joinRoom", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("offerIce", (ice, roomName) => {
+    socket.to(roomName).emit("offerIce", ice);
+  });
+  socket.on("answerIce", (ice, roomName) => {
+    socket.to(roomName).emit("answerIce", ice);
+  });
+});
 
 httpServer.listen(PORT, handleServerListen);
